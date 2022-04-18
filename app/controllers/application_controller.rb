df70baf9450b57093ea7f720564011e74ee5ad31
osha7 <ogroetz@gmail.com>
2022-04-18 00:10:05 +0000

    rock - erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    # redirect_to("https://www.wikipedia.org")
+    # render({ :plain => "Hello World!"}) #  renders plain text
+    # render({ :html => "<h1>Hello World!</h1>".html_safe }) #html_safe => first-> need it to render html, also -> security so 1 user can't inject malicious html into another user browser
+    render({ :template => "game_templates/user_rock.html.erb" }) # embedded ruby template
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
-
+  # get("", {:controller => "", :action => ""})
+  get("/rock", {:controller => "application", :action => "play_rock"})
 end
